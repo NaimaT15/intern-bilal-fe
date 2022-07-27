7e43912c0c149cfcc4baabbcbe9e411dfbbb1b7c
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2022-07-27 13:31:43 +0300

    added login component and add it to the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path:'',
     loadChildren:()=> import('./home/home.module').then(m=> m.HomeModule)
+  },
+  {
+    path:'login',
+    component:LoginComponent
   }
 ];
 
